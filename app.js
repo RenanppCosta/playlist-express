33b30ae75f71725cb1e356be4642dd36bb137375
Renanppcosta <renanppcosta@gmail.com>
2022-03-31 22:02:25 -0300

    Subindo as primeiras musicas com create

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-require("dotenv").config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const path = require("path");
+const Music = require("./model/Music");
 
 // importando banco de dados
-const connectToDb = require("./database/db")
+const connectToDb = require("./database/db");
 
-connectToDb()
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
 
-const port = process.env.PORT || 4000
+connectToDb();
+
+const port = process.env.PORT || 4000;
+
 app.get('/', (req, res)=> {
-  res.send('Hello World')
+  res.render("index");
+});
+
+app.get("/admin", (req,res)=>{
+  res.render("admin");
+});
+
+app.post("/create", async (req,res)=>{
+  const music = req.body;
+  await Music.create(music);
+  res.redirect("/");
 })
 
-app.listen(port, ()=>{console.log("Servidor rodando em http://localhost:${port")})
+app.listen(port, ()=>{console.log("Servidor rodando em http://localhost:${port")});
